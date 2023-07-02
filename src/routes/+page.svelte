0ec91adfb886d231586cdb7fa010bacb35376914
Danny Schantz <dschantz801@gmail.com>
2023-07-03 00:57:33 +0800

    trying out daisyUI components, very nice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,71 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/about">About my site</a>
-<button class="btn">Button</button>
-<span class="badge">Badge</span>
+<script>
+    // export let data 
+</script>
+
+<style>
+
+</style>
+<div class="grid grid-cols-2  px-10 gap-10">
+  <div class="card col-span-1 collapse collapse-arrow bg-primary shadow-primary">
+    <input type="checkbox" class="peer" /> 
+    <div class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+        <img class="rounded-xl " alt="hello" src="https://randomwordgenerator.com/img/picture-generator/57e3dc404d54aa14f1dc8460962e33791c3ad6e04e507440752f72d3954ac3_640.jpg"/>
+      Click me to show/hide content
+    </div>
+    <div class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda aut asperiores commodi in minima error, illo explicabo doloribus sequi cum, laudantium autem, voluptatem reiciendis id ut inventore repudiandae sed omnis?</p>
+    </div>
+  </div>
+  <div class="card collapse collapse-arrow bg-base-200">
+    <input type="checkbox" class="peer" /> 
+    <div class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+        <img class="rounded-xl " alt="hello" src="https://randomwordgenerator.com/img/picture-generator/57e0d1444250a414f1dc8460962e33791c3ad6e04e50744077297bd59445c2_640.jpg"/>
+      Click me to show/hide content
+    </div>
+    <div class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam esse tenetur illo, natus veritatis delectus voluptatem iusto necessitatibus similique nesciunt at impedit, commodi repudiandae culpa. Provident quis suscipit sint nesciunt?</p>
+    </div>
+  </div>
+  <div class="card collapse collapse-arrow bg-base-200">
+    <input type="checkbox" class="peer" /> 
+    <div class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+        <img class="rounded-xl " alt="hello" src="https://randomwordgenerator.com/img/picture-generator/57e3dc404d54aa14f1dc8460962e33791c3ad6e04e507440752f72d3954ac3_640.jpg"/>
+      Click me to show/hide content
+    </div>
+    <div class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"> 
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis dolorem aliquam laborum ratione quasi ipsa. Consectetur quod qui perspiciatis, quo ratione velit dignissimos, officiis ipsam cupiditate nostrum at sint iste?</p>
+    </div>
+  </div>
+  <div class="card collapse collapse-arrow bg-base-200">
+    <input type="checkbox" class="peer" /> 
+    <div class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+        <img class="rounded-xl " alt="hello" src="https://randomwordgenerator.com/img/picture-generator/57e3dc404d54aa14f1dc8460962e33791c3ad6e04e507440752f72d3954ac3_640.jpg"/>
+      Click me to show/hide content
+    </div>
+    <div class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"> 
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis dolorem aliquam laborum ratione quasi ipsa. Consectetur quod qui perspiciatis, quo ratione velit dignissimos, officiis ipsam cupiditate nostrum at sint iste?</p>
+    </div>
+  </div>
+  <div class="card collapse collapse-arrow bg-base-200">
+    <input type="checkbox" class="peer" /> 
+    <div class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+        <img class="rounded-xl " alt="hello" src="https://randomwordgenerator.com/img/picture-generator/57e3dc404d54aa14f1dc8460962e33791c3ad6e04e507440752f72d3954ac3_640.jpg"/>
+      Click me to show/hide content
+    </div>
+    <div class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"> 
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis dolorem aliquam laborum ratione quasi ipsa. Consectetur quod qui perspiciatis, quo ratione velit dignissimos, officiis ipsam cupiditate nostrum at sint iste?</p>
+    </div>
+  </div>
+  <div class="card collapse collapse-arrow bg-base-200">
+    <input type="checkbox" class="peer" /> 
+    <div class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+        <img class="rounded-xl " alt="hello" src="https://randomwordgenerator.com/img/picture-generator/57e3dc404d54aa14f1dc8460962e33791c3ad6e04e507440752f72d3954ac3_640.jpg"/>
+      Click me to show/hide content
+    </div>
+    <div class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"> 
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis dolorem aliquam laborum ratione quasi ipsa. Consectetur quod qui perspiciatis, quo ratione velit dignissimos, officiis ipsam cupiditate nostrum at sint iste?</p>
+    </div>
+  </div>
+</div>
+
+
