2fdc493aace87de3190726088c356bf509c10542
Danny Schantz <dschantz801@gmail.com>
2023-07-03 12:10:47 +0800

    playing around with styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,12 @@
 
 </style>
 <div class="grid grid-cols-2  px-10 gap-10">
-  <div class="card col-span-1 collapse collapse-arrow bg-primary shadow-primary">
+  <div class="card col-span-1 collapse collapse-arrow bg-gradient-to-r from-primary to-secondary shadow-primary">
     <input type="checkbox" class="peer" /> 
-    <div class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+    <div class="collapse-title bg-gradient-to-t from-primary to-secondary text-primary-content">
         <img class="rounded-xl " alt="hello" src="https://randomwordgenerator.com/img/picture-generator/57e3dc404d54aa14f1dc8460962e33791c3ad6e04e507440752f72d3954ac3_640.jpg"/>
-      Click me to show/hide content
     </div>
-    <div class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"> 
+    <div class="collapse-content bg-primary text-primary-content "> 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda aut asperiores commodi in minima error, illo explicabo doloribus sequi cum, laudantium autem, voluptatem reiciendis id ut inventore repudiandae sed omnis?</p>
     </div>
   </div>
@@ -20,7 +19,6 @@
     <input type="checkbox" class="peer" /> 
     <div class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
         <img class="rounded-xl " alt="hello" src="https://randomwordgenerator.com/img/picture-generator/57e0d1444250a414f1dc8460962e33791c3ad6e04e50744077297bd59445c2_640.jpg"/>
-      Click me to show/hide content
     </div>
     <div class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content"> 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam esse tenetur illo, natus veritatis delectus voluptatem iusto necessitatibus similique nesciunt at impedit, commodi repudiandae culpa. Provident quis suscipit sint nesciunt?</p>
