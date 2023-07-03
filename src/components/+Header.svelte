9709cc599c0eca11a0db55ce7a0da4e69a8aeb18
Danny Schantz <dschantz801@gmail.com>
2023-07-03 16:35:25 +0800

    added user can signin/signup

--- src/components/+Header.svelte
+++ src/components/+Header.svelte
@@ -1,5 +1,6 @@
 <script>
-    import { loggedIn } from '../utils/auth.js'
+    import { loggedIn, logOut } from '../utils/auth.js'
+    import { goto } from '$app/navigation'    
     let currentTheme;
     
     function toggleTheme() {
@@ -17,6 +18,9 @@
             document.documentElement.setAttribute("data-theme", "dracula");
         }
     }
+    function goToSignInPage() {
+        goto('/signin')
+    }
 
     updateTheme();
 </script>
@@ -25,27 +29,39 @@
       <div class="dropdown">
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
         <!-- svelte-ignore a11y-label-has-associated-control -->
-        <label tabindex="0" class="btn btn-ghost btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-        </label>
+        <label tabindex="0" class="btn btn-ghost">Countries</label>
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
         <ul tabindex="0" class="z-10 menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        {#if $loggedIn}
-          <li><a  href="/">Homepage</a></li>
-          <li><a href="/">About</a></li>
-          <li><a href="/">Sign Out</a></li>
-        {:else}
+            <li><a  href="/">Homepage</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Sign Out</a></li>
             <li><a  href="/">Homepage</a></li>
             <li><a href="/">About</a></li>
             <li><a href="/">Login</a></li>
             <li><a href="/">Sign Up</a></li>
-        {/if}
+        </ul>
+      </div>
+      <div class="dropdown">
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label tabindex="0" class="btn btn-ghost">Experience</label>
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <ul tabindex="0" class="z-10 menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a  href="/">Homepage</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Sign Out</a></li>
+            <li><a  href="/">Homepage</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Login</a></li>
+            <li><a href="/">Sign Up</a></li>
         </ul>
       </div>
     </div>
     <button href="/" class="btn btn-ghost gap-0 inline-flex text-lg md:text-2xl">
-        <span class="lowercase">side</span> 
-        <span class="text-primary lowercase first-letter:uppercase">quest</span>
+        <!-- <span class="lowercase">side</span> 
+        <span class="text-primary lowercase first-letter:uppercase">quest</span> -->
+        <span class="lowercase first-letter:uppercase">Discover</span>
+        <span class=" lowercase first-letter:uppercase">Fy</span>
     </button>
       
     <div class="navbar-end">
@@ -62,14 +78,11 @@
                 {/if}
             </label>
         </button>
-      <button class="btn btn-ghost btn-circle">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-      </button>
-      <button class="btn btn-ghost btn-circle">
-        <div class="indicator">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-          <span class="badge badge-xs badge-primary indicator-item"></span>
-        </div>
-      </button>
+        {#if !$loggedIn}
+            <button on:click={goToSignInPage} class="btn btn-ghost ">Sign In</button>
+            <button class="btn btn-ghost">Sign up</button>
+        {:else}
+            <button on:click={logOut} class="btn btn-ghost">Sign Out</button>
+        {/if}
     </div>
   </div>
